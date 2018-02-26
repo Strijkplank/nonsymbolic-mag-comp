2ab37a52d453d7b66bba280c09a95a534e1d1d2e
DrColling <lincoln@colling.net.nz>
2018-02-26 15:39:09 +0100

    updated DoKeyboardSetup.m

--- DoKeyboardSetup.m
+++ DoKeyboardSetup.m
@@ -1,9 +1,14 @@
 addpath([cd filesep 'functions'])
 addpath([cd filesep 'functions' filesep 'my-ptb-funcs'])
-if length(GetKeyboardIndices) == 1
-    deviceIndexToUse = GetKeyboardIndices;
+
+if ~IsLinux
+    if length(GetKeyboardIndices) == 1
+        deviceIndexToUse = GetKeyboardIndices;
+    else
+        [deviceNameToUse, deviceIndexToUse] = FindKeyboard();
+    end
 else
-    [deviceNameToUse, deviceIndexToUse] = FindKeyboard();
+    deviceIndexToUse = 'LINUX'
 end
 save('keyboard.ini','deviceIndexToUse','-ascii')
 disp('DONE!')
