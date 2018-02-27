dc0579d907feb4c49838cc54f60b8996f8a84694
lincoln colling <lincoln@colling.net.nz>
2018-02-27 18:56:54 +0100

    updated checksize

--- checksize.m
+++ checksize.m
@@ -1,7 +1,7 @@
 files  =dir([cd filesep 'error' filesep '*.mat']);
 
 file = [files.folder filesep files.name];
-
+load(file)
 screenWidth = errorData.rawInfo.d.screenXpixels;
 imageWidth = size(errorData.rawInfo.newImg,2);
 
